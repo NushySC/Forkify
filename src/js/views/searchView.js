ed05efc9c9999777581228208fcd098334cc53de
Anuska <ank.sampedro@gmail.com>
2019-07-07 16:29:55 +0200

    pagination

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,15 +1,23 @@
-import {
-    elements
-} from './base';
+import { elements } from './base';
 
 export const getInput = () => elements.searchInput.value;
 
-export const clearInput = () => { elements.searchInput.value = ''; 
+export const clearInput = () => {
+    elements.searchInput.value = '';
 };
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 };
+
+// export const highlightSelected = id => {
+//     const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+//     resultsArr.forEach(el => {
+//         el.classList.remove('results__link--active');
+//     });
+//     document.querySelector(`.results__link[href*="${id}"]`).classList.add('results__link--active');
+// };
 
 export const limitRecipeTitle = (title, limit = 30) => {
     const newTitle = [];
@@ -20,15 +28,16 @@ export const limitRecipeTitle = (title, limit = 30) => {
             }
             return acc + cur.length;
         }, 0);
+
         return `${newTitle.join(' ')} ...`;
     }
     return title;
 }
 
 const renderRecipe = recipe => {
-    const markup =
-        `<li>
-            <a class="results__link results__link--active" href=#"${recipe.recipe_id}">
+    const markup = `
+        <li>
+            <a class="results__link" href="#${recipe.recipe_id}">
                 <figure class="results__fig">
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
@@ -37,10 +46,43 @@ const renderRecipe = recipe => {
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
-        </li>`;
-        elements.searchResList.insertAdjacentHTML('beforeend', markup);
+        </li>
+    `;
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
+    let button;
+    if (page === 1 && pages > 1) {
+        button = createButton(page, 'next');
+    } else if (page < pages) {
+        button = `
+            ${createButton(page, 'prev')}
+            ${createButton(page, 'next')}
+        `;
+    } else if (page === pages && pages > 1) {
+        button = createButton(page, 'prev');
+    }
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    renderButtons(page, recipes.length, resPerPage);
 };
