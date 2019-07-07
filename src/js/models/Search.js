8f06b1b690395a83f36785f874b01a3e28bc140a
Anuska <ank.sampedro@gmail.com>
2019-07-07 18:58:27 +0200

    recipe

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { APIKey, proxy } from '../config';
 
 export default class Search {
     constructor(query) {
@@ -6,19 +7,11 @@ export default class Search {
     }
 
     async getResults() {
-        const APIKey = '25d102562bc4e7709d2b7714f2d6bcd4';
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
-
-        const searchURL = `${proxy}https://www.food2fork.com/api/search?key=${APIKey}&q=${this.query}`
-
-        //const recipeURL = `${proxy}https://www.food2fork.com/api/get?key=${APIKey}&q=${query}`
-
         try {
-            const res = await axios(searchURL);
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${APIKey}&q=${this.query}`);
             this.result = res.data.recipes;
-            //console.log(this.result)
         } catch (error) {
-            alert(error)
+            alert(error);
         }
     }
 }
