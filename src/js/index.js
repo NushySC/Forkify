30f29baa39db68e6537238bf5ed355b98e2dd005
Anuska <ank.sampedro@gmail.com>
2019-07-08 10:29:35 +0200

    list model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import {
@@ -86,6 +87,7 @@ const controlRecipe = async () => {
 };
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
 
 //Recipe btn clicks
 
